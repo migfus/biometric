ddc4b9496ceb534f5ef54f2e899b23dc9e1c144c
migfus20 <migfus20@gmail.com>
2026-06-30 16:36:55 +0800

    [Debug] change the build directory

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/',
     plugins: [
         laravel({
             input: 'resources/js/app.ts',
@@ -18,28 +17,9 @@ export default defineConfig({
             },
         }),
     ],
-    // LOCALHOST
     server: {
         watch: {
-            // Use polling on filesystems that don't support native events (Windows, VM, network shares)
             usePolling: true,
         },
     },
-    // SERVER
-    // server: {
-    //     host: '0.0.0.0',
-    //     port: 5173,
-
-    //     cors: {
-    //         origin: [
-    //             'http://192.168.24.21:8080',
-    //         ],
-    //         credentials: true,
-    //     },
-
-    //     hmr: {
-    //         host: '192.168.24.21',
-    //         port: 5173,
-    //     },
-    // }
 });
